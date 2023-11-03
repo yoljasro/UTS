1c791cddd3103ded6d122f812ed47ee3e9f8cc27
yoljasro <saidaliyevjasur450@gmail.com>
2023-11-03 11:41:04 +0500

    ico to png

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
       <Head>
         <title>UTS - Uzbekistan Tech Service</title>
 
-        <link rel="shortcut icon" href="/favicon.ico" /> 
+        <link rel="shortcut icon" href="/favicon.png" /> 
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/logo32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/logo16x16.png" />
